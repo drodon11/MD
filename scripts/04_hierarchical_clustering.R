be100395ac2554a09a87f1e0925a463a157fade6
krsnyk <david.krasnyak@estudiantat.upc.edu>
2026-03-16 17:50:24 +0100

    Eliminado la validación por Silhouette

--- scripts/04_hierarchical_clustering.R
+++ scripts/04_hierarchical_clustering.R
@@ -57,40 +57,11 @@ plot(res_hc, main = "Dendrograma de Vuelos (Métrica: Gower, Agregación: Ward)"
      xlab = "Vuelos", sub = "", ylab = "Distancia (Inercia)", cex = 0.5)
 
 # ------------------------------------------------------------------------------
-# Validación matemática de k mediante el Coeficiente de Silueta
-# ------------------------------------------------------------------------------
-cat("\n--- Evaluando diferentes k con el Coeficiente de Silueta ---\n")
-
-# Calculamos la silueta para k=2
-grupos_k2 <- cutree(res_hc, k = 2)
-sil_k2 <- silhouette(grupos_k2, mat_dist)
-media_k2 <- summary(sil_k2)$avg.width
-
-# Calculamos la silueta para k=3
-grupos_k3 <- cutree(res_hc, k = 3)
-sil_k3 <- silhouette(grupos_k3, mat_dist)
-media_k3 <- summary(sil_k3)$avg.width
-
-# Calculamos la silueta para k=4
-grupos_k4 <- cutree(res_hc, k = 4)
-sil_k4 <- silhouette(grupos_k4, mat_dist)
-media_k4 <- summary(sil_k4)$avg.width
-
-# Calculamos la silueta para k=5
-grupos_k5 <- cutree(res_hc, k = 5)
-sil_k5 <- silhouette(grupos_k5, mat_dist)
-media_k5 <- summary(sil_k5)$avg.width
-
-cat("Silueta media para k = 2:", media_k2, "\n")
-cat("Silueta media para k = 3:", media_k3, "\n")
-cat("Silueta media para k = 4:", media_k4, "\n")
-cat("Silueta media para k = 5:", media_k5, "\n")
-
-# ------------------------------------------------------------------------------
 # 10d. Discusión del número final de clústeres
 # ------------------------------------------------------------------------------
-# Tras observar los saltos de inercia en el dendrograma y usando el
-# método de Silhouette, definimos el punto de corte.
+# Tras observar los saltos de inercia en el dendrograma, definimos el punto de corte.
+# Usamos 3 clusters para poder discriminar mejor, 2 es poco.
+
 k_elegido <- 3
 
 # Dibujamos las fronteras de los grupos en el dendrograma para visualizar el corte
